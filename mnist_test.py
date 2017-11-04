b5e3022930b01fac520fbdf0e0b3fb58ca3edd91
arunponnusamy <6175828+arunponnusamy@users.noreply.github.com>
2017-11-04 13:31:39 +0530

    Add test input support

--- mnist_test.py
+++ mnist_test.py
@@ -1,5 +1,8 @@
 # import required modules
 import keras
+import sys
+import cv2
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 
@@ -29,7 +32,33 @@ model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accura
 model.load_weights('mnist_weights_epoch300.h5')
 
 # evaluate the model on test set 
-score = model.evaluate(x_test, y_test, batch_size=100)
+score = model.evaluate(x_test, y_test, batch_size=100, verbose=0)
 
-print("\nTest loss: ", score[0])
-print("Test accuracy: ", score[1])
+print("\nTest set loss: ", score[0])
+print("Test set accuracy: ", score[1])
+
+# load test input if provided
+if(len(sys.argv) == 2):
+    img = cv2.imread(sys.argv[1])
+    if(not img.data):
+        print("Could not load image")
+        exit
+
+    # preprocessing
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (28, 28), interpolation = cv2.INTER_AREA)
+    img = cv2.bitwise_not(img)
+    img = img.reshape(1, 784)
+    img = img.astype('float32')
+    img /= 255    
+
+    # predict the handwritten digit in the input image 
+    score = model.predict(img, batch_size=1, verbose=0)
+    
+    # display scores    
+    print("\nPrediction score for test input: " + sys.argv[1])
+    sort = sorted(range(len(score[0])), key=lambda k:score[0][k],reverse=True)
+    for index in sort:
+        print(str(index) + ": " + str(score[0][index]))  
+    percent = format(score[0][sort[0]] * 100, '.2f')
+    print("\nI am " + str(percent) + "% confident that it is " + str(sort[0]))
